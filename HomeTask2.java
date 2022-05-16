8f4e6e33518c74edd7e854630b812ea5ec9dcb7c
Kova <49371730+agbezrukov@users.noreply.github.com>
2022-05-16 21:23:44 +0600

    Java.Core.Level2.String
    Java.Core.Level2.String

--- HomeTask2.java
+++ HomeTask2.java
@@ -1,40 +1,52 @@
-package com.kova.lesson5;
+package com.kova.lesson7;
+
+import java.util.Arrays;
+
+import static java.lang.Integer.*;
 
 /**
- * Даны 3 переменные:
- * - operand1 (double)
- * - operand2 (double)
- * - operation (char ‘+’, ‘-’, ‘*’, ‘/’, ‘%’)
- * Написать функцию, которая принимает в качестве параметров эти три переменные и возвращает результат операции.
- * Протестировать функцию в main.
+ * Задание 2
+ * Дана строка с текстом, в котором есть цифры от 0 до 9.
+ * Написать 2 метода:
+ * - возвращающий массив цифр из переданной строки
+ * - возвращающий сумму цифр из переданного целочисленного массива
+ * Посчитать сумму всех чисел из строки
  * Например:
- * Параметры: operand1 = 24.4, operand2 = 10.1, operation = ‘+’
- * Результат: 34.5 (24.4 + 10.1)
+ * “Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?” -> 18 (8+1+2+4+3)
  */
+
 public class HomeTask2 {
     public static void main(String[] args) {
 
-        double operand1 = 0;
-        double operand2 = 2;
-        char operation = '.';
-
-        System.out.println(arithmeticOperation(operand1,operand2,operation));
+        String str = "Привет 8, как 1 2 твои дела? Может4, сделать 3 дело?";
+        System.out.println("Массив цифр - " + Arrays.toString(returnArrayNumbers(str)));
+        System.out.println("Сумма цифр массива: " + returnSumNumbers(returnArrayNumbers(str)));
     }
 
-    public static double arithmeticOperation(double operand1, double operand2, char operation){
 
-        if (operation == '+'){
-            return operand1 + operand2;
-        }else if (operation == '-'){
-            return operand1 - operand2;
-        }else if (operation == '*'){
-            return operand1 * operand2;
-        }else if (operation == '/'){
-            return operand1 / operand2;
-        }else if (operation == '%'){
-            return operand1 % operand2;
-        }else {
-            return 0;
+    private static int [] returnArrayNumbers(String str){
+
+        String result = str.replaceAll("[^0-9]","");
+
+        StringBuilder sb = new StringBuilder(result);
+
+        int [] numbers = new int[sb.length()];
+
+        for (int i = 0; i < sb.length(); i++) {
+            numbers[i] = parseInt(String.valueOf(sb.charAt(i)));
         }
+
+        return numbers;
+    }
+
+    private static int returnSumNumbers(int [] value){
+
+        int sum = 0;
+
+        for (int i = 0; i < value.length; i++) {
+            sum += value[i];
+        }
+
+        return sum;
     }
 }
